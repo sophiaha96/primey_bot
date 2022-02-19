ba9042f54324bccf5667575e7d723790e87a702f
Sophia Ha <sophia.ha@tetratrustcompany.com>
2022-02-19 10:34:03 -0500

    fix: leading 0s are invalid

--- utils/parse_input.py
+++ utils/parse_input.py
@@ -2,5 +2,5 @@ import re
 
 def parse_input(s: list):
     if len(s) == 1:
-        return int(s[0]) if bool(re.match("^[0-9]{5}$", s[0])) else -1
+        return int(s[0]) if bool(re.match("^([1-9][0-9]{4})$", s[0])) else -1
     return -1
